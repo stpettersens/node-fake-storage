569eda79c103c9737499403ded38117594d3a6be
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-03 17:22:29 +0100

    ...

---
+++ typings/fake-storage.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for fake-storage
+// Project: https://github.com/stpettersens/node-fake-storage
+// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "fake-storage" {
+	// count property - TODO
+	export function setItem(key: string, value: string): void;
+	export function getItem(key: string): string;
+	export function removeItem(key: string): void;
+	export function clear(): void;
+	export function key(index: number): string;
+	export function getItems(): string[];
+}
